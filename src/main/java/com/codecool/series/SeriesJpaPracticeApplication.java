cb989d3719a1dbc72a9b6391b9cd18f6aebee4ce
cortinadampezzo <kisgans@gmail.com>
2019-03-01 12:43:54 +0100

    change application properties, rename some classes

--- src/main/java/com/codecool/series/SeriesJpaPracticeApplication.java
+++ src/main/java/com/codecool/series/SeriesJpaPracticeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeriesJpaPracticeCortinadampezzoApplication {
+public class SeriesJpaPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeriesJpaPracticeCortinadampezzoApplication.class, args);
+        SpringApplication.run(SeriesJpaPracticeApplication.class, args);
     }
 
 }
