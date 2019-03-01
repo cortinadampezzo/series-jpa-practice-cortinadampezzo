1d39dbf69ac438f4b374fd0ad9df3b9931e683e0
cortinadampezzo <kisgans@gmail.com>
2019-03-01 12:30:20 +0100

    start project

---
+++ src/main/java/com/codecool/series/SeriesJpaPracticeCortinadampezzoApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.series;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesJpaPracticeCortinadampezzoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesJpaPracticeCortinadampezzoApplication.class, args);
+    }
+
+}
